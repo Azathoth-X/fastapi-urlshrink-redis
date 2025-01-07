39a0c7aaf2f5b60f46fdf423758cad09a4c1e0c4
Azathoth-X <ranaprikshit25@gmail.com>
2025-01-07 23:21:20 +0530

    bruh

--- app/app.py
+++ app/app.py
@@ -13,9 +13,9 @@ def ran_gen(chars=string.ascii_uppercase+string.digits):
     return "".join(random.choice(chars) for i in range(5))
 
 
-@app.get("/{url}")
-def get_url(url:str):
-    return dic[url] 
+@app.get("/{short}")
+def get_url(short:str):
+    return dic[short] 
 
 @app.post("/",status_code=status.HTTP_201_CREATED)
 def create_url(tmpurl:url):
